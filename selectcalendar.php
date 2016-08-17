fd049864914a3e630a0e46822071277975553819
xiaocuihero <xiaocuihero@163.com>
2016-08-17 09:38:32 +0800

    _____bug: while in calendar user input charactor ',' will casue display error in calendarlist
    replace ',' to a 10 charactor random string.

--- selectcalendar.php
+++ selectcalendar.php
@@ -6,18 +6,18 @@ $con = mysqli_connect('localhost', 'sa', '123456', 'calendardb');
 $para="";
 mysqli_query($con,"set names 'utf8'");
 if (!$con)
-	{
-		die('Could not connect: ' . mysqli_error());
-	}
+{
+	die('Could not connect: ' . mysqli_error());
+}
 
- $sql = "SELECT u.realname,group_concat(concat(p.projectname,'：',c.`Subject`)separator '<br/><br/>') FROM `calendar` c left join `user` u on c.UPAccount=u.id left join `project` p on p.id=c.Location where c.StartTime='".$dt1."' group by c.`UPAccount`";
+$sql = "SELECT u.realname,group_concat(concat(p.projectname,'：',c.`Subject`)separator '<br/><br/>') FROM `calendar` c left join `user` u on c.UPAccount=u.id left join `project` p on p.id=c.Location where c.StartTime='".$dt1."' group by c.`UPAccount`";
 $result=mysqli_query($con,$sql);
-  if($result !=null)
-  {
-  while ($row = mysqli_fetch_row($result)) {
-	$para.=$row[0].','.$row[1].'||';
+if($result !=null)
+{
+	while ($row = mysqli_fetch_row($result)) {
+		$para.=$row[0].'8FyRQoC7xr'.$row[1].'||';
 	}
-  }
+}
 mysqli_close($con);
 echo $para;
 
